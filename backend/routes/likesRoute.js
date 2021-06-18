cc85473cdf3ad31df5fcdf3e386a1fb9e46fae91
Philippe DAVID <philippedavid.phd@gmail.com>
2021-06-18 17:36:00 +0200

    routes: suppression likesRoute.js

--- backend/routes/likesRoute.js
+++
@@ -1,8 +0,0 @@
-// const express = require('express');
-// const router = express.Router();
-// const likesCtrl = require('../controllers/likesCtrl'); 
-
-// router.post('/:messageId/like', likesCtrl.likeMessage);        // Like message
-// router.post('/:messageId/dislike', likesCtrl.dislikeMessage);     // disLike message
-
-// module.exports = router;
